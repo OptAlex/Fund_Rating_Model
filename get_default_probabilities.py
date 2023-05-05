a7d7e6633eecbebddd2e5ac8310f9c46d48a30e7
Tim Müller <timmuller@MacBook-Pro-von-Tim.local>
2023-05-05 15:07:05 +0200

    added some stuff, just to play around

--- get_default_probabilities.py
+++ get_default_probabilities.py
@@ -1,5 +1,7 @@
 import pandas as pd
-from help_functions.estimate_default_prob import estimate_default_prob
+from help_functions.estimate_default_prob import estimate_historical_stat_default_prob, estimate_historical_count_default_prob, estimate_mc_stat_default_prob, estimate_mc_count_default_prob
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 # Load data into DataFrame, these steps are only for the dummy data
 df = (
@@ -9,15 +11,46 @@ df = (
     .drop("index", axis=1)
     .iloc[:300]
 )
-df[["return_agg", "return_spy", "return_mergaai"]] = (
-    df[["return_agg", "return_spy", "return_mergaai"]] * 100
-)
 
 # Call function to estimate default probabilities
-default_prob = estimate_default_prob(
-    df=df, threshold=0.1, num_samples=1, window_size=252, step=1, score="t_score"
+estimate_mc_count_default_prob = estimate_mc_count_default_prob(
+    df=df, threshold=-0.005, num_samples=50, prediction_days=30
 )
+"""estimate_historical_count_default_prob = estimate_historical_count_default_prob(
+    df=df, threshold=-0.01, window_size=30, step=1,
+)
+estimate_historical_stat_default_prob = estimate_historical_stat_default_prob(
+    df=df, threshold=-0.1, window_size=30, step=1, score="t_score"
+)
+estimate_mc_stat_default_prob = estimate_mc_stat_default_prob(
+    df=df, threshold=-0.03, num_samples=50, prediction_days=30, score="t_score"
+)
+estimate_mc_stat_default_prob = estimate_mc_count_default_prob(
+    df=df, threshold=-0.005, num_samples=50, prediction_days=30
+)"""
 
 # Print default probabilities
-print(default_prob)
+print(estimate_mc_count_default_prob)
 
+
+# create a figure with three boxplots
+fig, ax = plt.subplots(figsize=(7, 4))
+sns.boxplot(data=estimate_mc_count_default_prob, ax=ax, palette='PuBu_r', showfliers=True, width=0.3, whis=[5, 95])
+ax.set_title('Default Probability Forecast (30 days) for Each Fund', fontsize=14)
+ax.set_xlabel('Fund', fontsize=12)
+ax.set_ylabel('Default Probability', fontsize=12)
+ax.tick_params(labelsize=10)
+
+# save the figure
+fig.savefig('mc_forecast_boxplot_fund.png', dpi=300, bbox_inches='tight')
+
+# create a figure with one boxplot
+fig, ax = plt.subplots(figsize=(4, 4))
+sns.boxplot(data=estimate_mc_count_default_prob.stack().reset_index(level=1, drop=True), ax=ax, color='lightsteelblue', showfliers=True, width=0.1, whis=[5, 95])
+ax.set_title('Default Probability Forecast (30 days) for All Funds', fontsize=14)
+ax.set_xlabel('All Funds', fontsize=12)
+ax.set_ylabel('Default Probability', fontsize=12)
+ax.tick_params(labelsize=10)
+
+# save the figure
+fig.savefig('mc_forecast_boxplot_all_funds.png', dpi=300, bbox_inches='tight')
--- help_functions/estimate_default_prob.py
+++ help_functions/estimate_default_prob.py
@@ -2,11 +2,16 @@ import pandas as pd
 import numpy as np
 from arch import arch_model
 from scipy.stats import norm, t
+from datetime import timedelta
 
-
+############################################################################################
+# Help functions for the estimation of the default probabilities
+############################################################################################
 def get_fund_dict(df: pd.DataFrame) -> dict:
     """
     Create a dictionary of all funds with price & returns.
+    :param df: df with the historical prices and returns
+    :return: dictionary with the fund name and the historical prices and returns of the fund
     """
     fund_dict = {}
     for col in df.columns:
@@ -18,27 +23,61 @@ def get_fund_dict(df: pd.DataFrame) -> dict:
     return fund_dict
 
 
-def estimate_default_prob(
+def simulate_returns(garch_parameters, num_days_to_simulate):
+    """
+    Function to simulate the future returns
+    :param garch_parameters: alpha, beta, sigma
+    :param num_days_to_simulate: number of days on which the simulation is performed
+    :return: simulated returns for the number of prediction days
+    """
+    sim_mod = arch_model(None, p=1, q=1, rescale="False")
+    sim_data = sim_mod.simulate(garch_parameters, num_days_to_simulate)
+    return sim_data["data"]
+
+
+def run_garch_model(returns):
+    """
+    Simple function to run a GARCH model
+    :param returns: returns on which to run the model
+    :return: the residuals of the model
+    """
+    # using GARCH(1,1)
+    am = arch_model(returns, p=1, q=1, rescale="False")
+    res = am.fit(disp="off")
+    return res
+
+############################################################################################
+# Backtest of the default probabilites
+############################################################################################
+def estimate_historical_stat_default_prob(
     df: pd.DataFrame,
     threshold: float,
-    num_samples: int,
     window_size: int,
     step: int,
     score: str,
 ) -> pd.DataFrame:
-    """Estimate the probability to default for each fund.
-
-    To estimate the probability to default for each fund a rowling window approach is used. For bootstrapping
-    the number of samples can be specified as needed. The probability to default is calculated by using the z- or t- score
-    (depends on the assumption).
     """
+    Estimate the probability to default for each fund by using a statistical approach.
 
+    To estimate the probability to default for each fund a rowling window approach is used. The probability to default is calculated by using the z- or t- score
+    (depends on the assumption).
+
+    :param df: df with the historical prices and returns
+    :param threshold: threshold which defines the default of a fund
+    :param window_size: defines the days of the rolling window
+    :param step: steps wo roll on in the rolling window
+    :param score: assumed distribution of the returns
+    :return: df with the historical default probabilities
+    """
     default_prob_dict = {}
     date = np.array(df["date"].values)
     fund_dict = get_fund_dict(df)
 
     for fund_name, (prices, returns) in fund_dict.items():
         default_prob = np.zeros(len(df))
+
+        # multiply returns by 100 as this is prefered by the GARCH package
+        returns *= 100
 
         for i in range(0, len(df) - window_size, step):
             start_idx = i
@@ -54,37 +93,17 @@ def estimate_default_prob(
             beta = results.params["beta[1]"]
             sigma2 = results.conditional_volatility**2
 
-            bootstrap_returns = np.zeros((num_samples, len(returns)))
-            for j in range(num_samples):
-                bootstrap_start_idx = np.random.randint(
-                    start_idx, end_idx - window_size + 1
-                )
-                bootstrap_end_idx = bootstrap_start_idx + window_size
-                bootstrap_returns[j, :] = fund_dict[fund_name][1][
-                    bootstrap_start_idx:bootstrap_end_idx
-                ]
-
-            bootstrap_volatility = np.zeros(num_samples)
-            for j in range(num_samples):
-                bootstrap_model = arch_model(
-                    bootstrap_returns[j, :], p=1, q=1, rescale=False
-                )
-                bootstrap_results = bootstrap_model.fit(disp="off")
-                bootstrap_volatility[j] = (
-                    bootstrap_results.forecast(horizon=1, reindex=False)
-                    .variance.iloc[-1]
-                    .item()
-                )
-
             for j in range(start_idx, end_idx):
                 if j == start_idx:
                     start_value = fund_dict[fund_name][0][j]
                     current_value = start_value
-                    current_volatility = sigma2[j]
                     current_date = df["date"][j]
                 else:
                     current_value = fund_dict[fund_name][0][j]
                     current_date = df["date"][j]
+
+                # get the corresponding sigma2 value for j by adjusting for the start index
+                current_volatility = sigma2[j - start_idx]
 
                 default_value = start_value * (1 - threshold)
                 if score == "z_score":
@@ -101,11 +120,229 @@ def estimate_default_prob(
                     default_prob[j] += 1 - t.cdf(t_score, window_size - 1)
                 date[j] = current_date
 
-            default_prob[start_idx:end_idx] /= num_samples
-
-        default_prob_dict[fund_name] = default_prob
+        # Divide the default prob by 100 as we multiplied the returns earlier
+        default_prob_dict[f"default_prob_{fund_name}"] = default_prob / 100
 
     default_prob_df = pd.DataFrame(default_prob_dict)
     default_prob_df["date"] = date
     default_prob_df = default_prob_df.set_index("date")
+    # ToDo add a confidence intervall
     return default_prob_df
+
+
+def estimate_historical_count_default_prob(
+        df: pd.DataFrame,
+        threshold: float,
+        window_size: int,
+        step: int,
+) -> pd.DataFrame:
+    """
+    Estimate the probability to default for each fund by using a statistical approach.
+
+    To estimate the probability to default for each fund a rowling window approach is used. The probability to default is calculated by using the z- or t- score
+    (depends on the assumption).
+
+    :param df: df with the historical prices and returns
+    :param threshold: threshold which defines the default of a fund
+    :param window_size: defines the days of the rolling window
+    :param step: steps wo roll on in the rolling window
+    :param score: assumed distribution of the returns
+    :return: df with the historical default probabilities
+    """
+    default_prob_dict = {}
+    date = np.array(df["date"].values)
+    fund_dict = get_fund_dict(df)
+
+    for fund_name, (prices, returns) in fund_dict.items():
+        default_prob = np.zeros(len(df))
+
+        # multiply returns by 100 as this is prefered by the GARCH package
+        returns *= 100
+
+        for i in range(0, len(df) - window_size, step):
+            start_idx = i
+            end_idx = i + window_size
+            if end_idx > len(df):
+                break
+
+            returns = fund_dict[fund_name][1][start_idx:end_idx]
+
+            for j in range(len(returns)):
+                # Calculate the threshold violations
+                if returns[j] / 100 <= -threshold:
+                    default_prob[j] += 1
+                    print(default_prob)
+
+            # Divide the default prob by 100 as we multiplied the returns earlier
+            default_prob_dict[f"default_prob_{fund_name}"] = default_prob / 100
+
+    default_prob_df = pd.DataFrame(default_prob_dict)
+    default_prob_df["date"] = date
+    default_prob_df = default_prob_df.set_index("date")
+    # ToDo add a confidence intervall
+    return default_prob_df
+
+############################################################################################
+# Forecast of the default probabilites
+############################################################################################
+def estimate_mc_stat_default_prob(
+    df: pd.DataFrame,
+    threshold: float,
+    num_samples: int,
+    prediction_days: int,
+    score: str,
+) -> pd.DataFrame:
+    """
+    Perform a Monte Carlo Simulation and estimate the probability to default for each fund by using a statistical approach.
+
+    The future default probability is estimated on future returns from a Monte Carlo Simulation. For bootstrapping
+    the number of samples can be specified as needed. The probability to default is calculated by using the z- or t- score
+    (depends on the assumption).
+    :param df: df with the historical prices and returns
+    :param threshold: threshold which defines the default of a fund
+    :param num_samples: number of samples to bootstrap the data
+    :param prediction_days: defines the number of days to estimate the default probabilities
+    :param score: assumed distribution of the returns
+    :return: df with the predicted default probabilities
+    """
+    default_prob_dict = {}
+    fund_dict = get_fund_dict(df)
+
+    last_day = pd.to_datetime(df["date"].iloc[-1])
+    end_date = last_day + timedelta(days=prediction_days - 1)
+
+    for fund_name, (prices, returns) in fund_dict.items():
+        default_prob = np.zeros(prediction_days)
+
+        # Multiply returns by 100 as this is preferred by the GARCH package
+        returns *= 100
+
+        # Fit the GARCH model to the returns
+        res = run_garch_model(returns)
+        garch_parameters = res.params
+
+        for i in range(num_samples):
+            # Simulate future returns using the GARCH model
+            simulated_returns = simulate_returns(
+                garch_parameters, num_days_to_simulate=prediction_days
+            )
+
+            # Calculate the conditional volatility for the simulated returns
+            sim_mod = arch_model(simulated_returns, p=1, q=1, rescale="False")
+            sim_res = sim_mod.fit(disp="off")
+            alpha = sim_res.params["alpha[1]"]
+            beta = sim_res.params["beta[1]"]
+            sigma2 = sim_res.conditional_volatility**2
+
+            # Calculate the default probability for each day in the future
+            start_value = prices[-1]
+            for j in range(prediction_days):
+                current_value = start_value * (1 + simulated_returns[j] / 100)
+                current_volatility = sigma2[j]
+                default_value = start_value * (1 - threshold)
+
+                if score == "z_score":
+                    z_score = (
+                        np.log(default_value / current_value)
+                        + (alpha + beta * current_volatility) / 2
+                    ) / np.sqrt(current_volatility)
+                    default_prob[j] += norm.sf(z_score)
+                else:
+                    t_score = (
+                        np.log(default_value / current_value)
+                        + (alpha + beta * current_volatility) / 2
+                    ) / np.sqrt(current_volatility / prediction_days)
+                    default_prob[j] += 1 - t.cdf(t_score, prediction_days - 1)
+
+                # Update the start value for the next day
+                start_value = current_value
+
+        # Divide the default prob by the number of samples
+        default_prob /= num_samples
+
+        # Add the default probabilities for the fund to the dictionary
+        # Divide the default prob by 100 as we multiplied the returns earlier
+        default_prob_dict[f"default_prob_{fund_name}"] = default_prob / 100
+
+    # Create a date range for the specified days
+    date_range = pd.date_range(start=last_day, end=end_date)
+
+    # Create a pandas dataframe to store the default probabilities
+    default_prob_df = pd.DataFrame(default_prob_dict, index=date_range)
+
+    # Resample the dataframe to get the default probabilities for each day
+    daily_default_prob_df = default_prob_df.resample("D").asfreq()
+
+    # Forward fill missing values
+    daily_default_prob_df.fillna(method="ffill", inplace=True)
+
+    # Return the default probabilities for the next month
+    return daily_default_prob_df
+
+
+def estimate_mc_count_default_prob(
+    df: pd.DataFrame,
+    threshold: float,
+    num_samples: int,
+    prediction_days: int,
+) -> pd.DataFrame:
+    """
+    Perform a Monte Carlo Simulation and estimate the probability to default for each fund by counting the viaolations.
+
+    The future default probability is estimated on future returns from a Monte Carlo Simulation. For bootstrapping
+    the number of samples can be specified as needed. The probability to default is calculated by counting the violations
+    of the specified threshold.
+    :param df: df with the historical prices and returns
+    :param threshold: threshold which defines the default of a fund
+    :param num_samples: number of samples to bootstrap the data
+    :param prediction_days: defines the number of days to estimate the default probabilities
+    :return: df with the predicted default probabilities
+    """
+    default_prob_dict = {}
+    fund_dict = get_fund_dict(df)
+
+    last_day = pd.to_datetime(df["date"].iloc[-1])
+    end_date = last_day + timedelta(days=prediction_days - 1)
+
+    for fund_name, (prices, returns) in fund_dict.items():
+        default_prob = np.zeros(prediction_days)
+
+        # Multiply returns by 100 as this is preferred by the GARCH package
+        returns *= 100
+
+        # Fit the GARCH model to the returns
+        res = run_garch_model(returns)
+        garch_parameters = res.params
+
+        for i in range(num_samples):
+            # Simulate future returns using the GARCH model
+            simulated_returns = simulate_returns(
+                garch_parameters, num_days_to_simulate=prediction_days
+            )
+
+            for j in range(prediction_days):
+                # Calculate the threshold violations
+                if simulated_returns[j] / 100 <= -threshold:
+                    default_prob[j] += 1
+
+        # Divide the default prob by the number of samples
+        default_prob /= num_samples
+
+        # Add the default probabilities for the fund to the dictionary
+        # Divide the default prob by 100 as we multiplied the returns earlier
+        default_prob_dict[f"default_prob_{fund_name}"] = default_prob / 100
+
+    # Create a date range for the specified days
+    date_range = pd.date_range(start=last_day, end=end_date)
+
+    # Create a pandas dataframe to store the default probabilities
+    default_prob_df = pd.DataFrame(default_prob_dict, index=date_range)
+
+    # Resample the dataframe to get the default probabilities for each day
+    daily_default_prob_df = default_prob_df.resample("D").asfreq()
+
+    # Forward fill missing values
+    daily_default_prob_df.fillna(method="ffill", inplace=True)
+
+    # Return the default probabilities for the next month
+    return daily_default_prob_df
