aba6e86b9b07f66f96e5d4c8410866b743c6b050
OptAlex <82665203+OptAlex@users.noreply.github.com>
2023-05-08 22:50:02 +0200

    pseudo process, still working on it

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from help_functions.help_data_transformation import input_data, log_returns
+from help_functions.help_data_transformation import create_log_returns
+from help_functions.help_functions_default_prob import calc_threshold_violation
 import pandas as pd
 from const import *
 
 
-# generate log return df for R
-excel_data = pd.read_excel(PATH_DATA + "Second Fund list.xlsx")
-data = input_data(excel_data)
-log_returns = log_returns(data)
-log_returns.to_csv(PATH_DATA+"log_returns.csv")
+df_log_returns = create_log_returns("ETF_List.xlsx")
+
+# Copula model
+#
+# df_sim_returns = pd.read_csv(File with simulated returns from R)
+#
+# df_all_violations = pd.DataFrame()
+# for thold in [-100, -75, -50, -35, -25, -15, -10, -5]:
+#   df_threshold_violations = calc_threshold_violation(df_sim_returns, threshold = thold)
+#   df_all_violations = pd.concat[df_all_violations, df_threshold_violations]
