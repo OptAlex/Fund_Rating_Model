479f18108b7893be495bec55834eeb95e67f61da
OptAlex <82665203+OptAlex@users.noreply.github.com>
2023-05-08 22:30:42 +0200

    change help functions, simplifying

--- help_functions/help_data_transformation.py
+++ help_functions/help_data_transformation.py
@@ -1,11 +1,12 @@
-# Example file for creating help functions which can be used in the whole project
+# help functions related to data transformation
 from const import *
 import pandas as pd
 import numpy as np
 
+
 def input_data(df, date_column="Date"):
     """
-    outputs correctly formatted data to perform operation.
+    Outputs correctly formatted data to perform operations.
     :param date_column: date column name
     :param df: df from source, i.e. excel
     :return: correctly formatted df
@@ -13,15 +14,43 @@ def input_data(df, date_column="Date"):
     df[date_column] = pd.to_datetime(df[date_column], format="%Y-%M-%D")
     df_indexed = df.set_index(df[date_column])
     df_indexed = df_indexed.drop(columns=[date_column])
-    df_dropped = df_indexed.dropna()
+    df_sorted = df_indexed.sort_index(ascending=True)
+    df_dropped = df_sorted.dropna()
 
     return df_dropped
 
-def log_returns(df):
-    df_returns = df.sort_index(ascending=True).pct_change().dropna()
-    df_log_returns = np.log(1+df_returns)
+
+def create_log_returns(str_raw_data_name, bool_drop_date=True):
+    """
+    Create log returns out of historical prices time series.
+    :param str_raw_data_name: name of the Excel file
+    :param bool_drop_date: if true, drop date column
+    :return: dataframe with col = tickers, rows = days, values = log returns
+    """
+
+    df_raw_data = pd.read_excel(PATH_DATA + str_raw_data_name)
+    df_data = input_data(df_raw_data)
+
+    df_log_returns = np.log(df_data / df_data.shift(1))
+    df_log_returns = df_log_returns[1:]
+
+    if bool_drop_date:
+        df_log_returns = df_log_returns.reset_index(drop=True)
 
     # Extract the last string from each column name and rename the column
-    df_log_returns.rename(columns=lambda x: "log_returns_" + x.split()[-1], inplace=True)
+    df_log_returns.rename(columns=lambda x: x.split()[-1], inplace=True)
 
     return df_log_returns
+
+
+def get_fund_dict(df: pd.DataFrame) -> dict:
+    """
+    Create a dictionary of all funds with returns.
+    :param df: df with the historical returns
+    :return: dictionary with the fund name and the historical returns of the fund
+    """
+    fund_dict = {}
+    for col in df.columns:
+        returns = df[col].values
+        fund_dict[col] = returns
+    return fund_dict
--- help_functions/help_functions_default_prob.py
+++ help_functions/help_functions_default_prob.py
@@ -1,69 +1,27 @@
+# help functions to calculate the default probabilities
+
 import pandas as pd
 import numpy as np
-from arch import arch_model
-from scipy.stats import norm
+from help_functions.help_data_transformation import get_fund_dict
 
 
-def get_fund_dict(df: pd.DataFrame) -> dict:
+def calc_threshold_violation(df, threshold):
     """
-    Create a dictionary of all funds with price & returns.
-    :param df: df with the historical prices and returns
-    :return: dictionary with the fund name and the historical prices and returns of the fund
+    Calculate if a fund violates the threshold.
+    :param df: df with the historical returns
+    :param threshold: threshold which defines the default of a fund; default if <; i.e. -0.1 for 10 percent loss
+    :return: df with 1, if violated, 0 else
     """
-    fund_dict = {}
-    for col in df.columns:
-        if col.startswith("price_"):
-            fund_name = col.split("_")[1]
-            prices = df[col].values
-            returns = df[f"return_{fund_name}"].values
-            fund_dict[fund_name] = (prices, returns)
-    return fund_dict
+    fund_dict = get_fund_dict(df)
+    dict_violations = {fund_name: [] for fund_name in fund_dict.keys()}
 
+    for fund_name, returns in fund_dict.items():
+        arr_cumulative_returns = np.cumsum(returns)
 
-def simulate_returns(garch_parameters: list, num_days_to_simulate: int) -> list:
-    """
-    Function to simulate the future returns
-    :param garch_parameters: alpha, beta, sigma
-    :param num_days_to_simulate: number of days on which the simulation is performed
-    :return: simulated returns for the number of prediction days
-    """
-    sim_mod = arch_model(None, p=1, q=1, rescale="False")
-    sim_data = sim_mod.simulate(garch_parameters, num_days_to_simulate)
-    return sim_data["data"]
+        # 1 if threshold strictly violated, 0 else
+        binary_violations = np.sum(np.any(arr_cumulative_returns < threshold))
+        dict_violations[fund_name].append(binary_violations)
 
+    df_defaults = pd.DataFrame(dict_violations)
 
-def run_garch_model(returns: list) -> list:
-    """
-    Simple function to run a GARCH model
-    :param returns: returns on which to run the model
-    :return: the residuals of the model
-    """
-    # using GARCH(1,1)
-    am = arch_model(returns, p=1, q=1, rescale="False")
-    res = am.fit(disp="off")
-    return res
-
-def get_ci_from_df(df: pd.DataFrame, confidence_level: float) -> dict:
-    """
-    Calculate the mean and confidence interval for the portfolio based on a DataFrame with default probabilities.
-    :param df: DataFrame with default probabilities for the portfolio.
-    :param confidence_level: Desired confidence level.
-    :return: Dictionary containing the mean and confidence interval for the portfolio.
-    """
-    n = len(df)
-    z_score = norm.ppf(1 - (1 - confidence_level) / 2)
-    mean_dict = {}
-    ci_dict = {}
-
-    for column in df.columns:
-        data = df[column].to_numpy()
-        mean = np.mean(data)
-        std_error = np.std(data, ddof=1) / np.sqrt(n)
-        margin_of_error = z_score * std_error
-        lower_ci = mean - margin_of_error
-        upper_ci = mean + margin_of_error
-
-        mean_dict[column] = mean
-        ci_dict[column] = (lower_ci, upper_ci)
-
-    return {'mean': mean_dict, 'confidence_interval': ci_dict}
+    return df_defaults
