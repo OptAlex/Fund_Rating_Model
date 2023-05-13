a1670c06d67e34979edff9dab5ce913e2230c3e9
OptAlex <82665203+OptAlex@users.noreply.github.com>
2023-05-13 23:42:59 +0200

    bootstrapping implemented

--- main_par_fucntions.py
+++ main_par_fucntions.py
@@ -7,21 +7,18 @@ from help_functions.run_R_code import call_r
 import numpy as np
 import multiprocessing
 
-df_hist_log_returns = create_log_returns("data/ETF_List_short.xlsx")
-SIM_NUMBER = 3
-THRESHOLDS = [0.01, 0.03, 0.05, 0.075, 0.10, 0.125, 0.15, 0.20] # change to log
+df_hist_log_returns = create_log_returns("data/ETF_List.xlsx")
+SIM_NUMBER = 1000
+THRESHOLDS = [0.01, 0.03, 0.05, 0.075, 0.10, 0.125, 0.15, 0.20]
 THRESHOLDS_LOG = np.log([x + 1 for x in THRESHOLDS])
 CVAR_LEVEL = [0.1, 0.5, 1, 5, 10]
-
-# Create a bootstrapped sample of the log returns.
-bootstrapped_returns = bootstrap_returns(df=df_hist_log_returns, bootstrap_samples=1, bootstrap_days=252)
-
+NUM_BOOTSTRAPPING = 100
 
 # Define the function to be executed in parallel
-def run_simulation(number_simulation):
+def run_simulation(number_simulation, data):
     arr_sim_log_returns = call_r(
         script_content=script_content,
-        inputs={"df_log_returns": df_hist_log_returns},
+        inputs={"data": data},
         output_var="simulated_returns",
     )
     df_sim_log_returns = pd.DataFrame(
@@ -107,73 +104,85 @@ def calculate_default_probabilities_wrapper(sim_results, thresholds, sim_number)
 
 
 if __name__ == "__main__":
-    # Determine the number of processes to use
-    num_processes = multiprocessing.cpu_count()
+    df_all_CVaR = pd.DataFrame()
+    df_all_default_prob_port = pd.DataFrame()
+    for i in range(NUM_BOOTSTRAPPING):
 
-    # Create a Pool object
-    pool = multiprocessing.Pool(processes=num_processes)
+        # Create a bootstrapped sample of the log returns.
+        bootstrapped_returns = bootstrap_returns(df=df_hist_log_returns)
 
-    # Perform the simulation once
-    sim_results = pool.map(run_simulation, range(SIM_NUMBER))
+        # Determine the number of processes to use
+        num_processes = multiprocessing.cpu_count()
 
-    # Close the Pool
-    pool.close()
-    pool.join()
+        # Create a Pool object
+        pool = multiprocessing.Pool(processes=num_processes)
 
-    lst_ticker_column_names = sim_results[0].columns.tolist()
+        # Perform the simulation once
+        sim_results = pool.starmap(run_simulation, [(x, bootstrapped_returns) for x in range(SIM_NUMBER)])
 
-    print("Done with simulating log returns and threshold verification.")
+        # Close the Pool
+        pool.close()
+        pool.join()
 
-    df_default_probabilities = calculate_default_probabilities_wrapper(sim_results, THRESHOLDS_LOG, SIM_NUMBER)
+        lst_ticker_column_names = sim_results[0].columns.tolist()
 
-    # Convert log returns to standard returns and store the converted log returns
-    # converted_returns_dict = {}
+        print("Done with simulating log returns and threshold verification.")
 
-    df_all_weighted_avg_log_returns = pd.DataFrame()
-    df_all_weighted_avg_standard_returns = pd.DataFrame()
-    for i, sim_result in enumerate(sim_results):
-        # Convert log returns to standard returns
-        df_standard_returns = convert_returns(sim_result, bool_to_log=False)
+        df_default_probabilities = calculate_default_probabilities_wrapper(sim_results, THRESHOLDS_LOG, SIM_NUMBER)
 
-        # Calculate weighted average across each row, mean because equally weighted!
-        df_weighted_avg_standard_returns = calculate_portfolio_return(df_standard_returns)
+        # Convert log returns to standard returns and store the converted log returns
+        # converted_returns_dict = {}
 
-        # Store the weighted average in a variable
-        # var_name = f"df_standard_port_returns_{i + 1}"
-        #globals()[var_name] = df_weighted_avg_standard_returns
+        df_all_weighted_avg_log_returns = pd.DataFrame()
+        df_all_weighted_avg_standard_returns = pd.DataFrame()
+        for i, sim_result in enumerate(sim_results):
+            # Convert log returns to standard returns
+            df_standard_returns = convert_returns(sim_result, bool_to_log=False)
 
-        # Add the weighted average to the dictionary
-        # converted_returns_dict[var_name] = df_weighted_avg_standard_returns
+            # Calculate weighted average across each row, mean because equally weighted!
+            df_weighted_avg_standard_returns = calculate_portfolio_return(df_standard_returns)
 
-        # Convert standard returns back to log returns
-        df_weighted_avg_log_returns = convert_returns(df_weighted_avg_standard_returns)
+            # Store the weighted average in a variable
+            # var_name = f"df_standard_port_returns_{i + 1}"
+            #globals()[var_name] = df_weighted_avg_standard_returns
 
-        # aggregate all simulated portfolio log returns into one df
-        sim_name = f"Simulation {i + 1}"
-        df_all_weighted_avg_log_returns = pd.concat([df_all_weighted_avg_log_returns, df_weighted_avg_log_returns],
-                                                    axis=1)
-        df_all_weighted_avg_log_returns.rename(columns={0: sim_name}, inplace=True)
+            # Add the weighted average to the dictionary
+            # converted_returns_dict[var_name] = df_weighted_avg_standard_returns
 
-        # aggregate all simulated portfolio standard returns into one df
-        df_all_weighted_avg_standard_returns = pd.concat([df_all_weighted_avg_standard_returns, df_weighted_avg_standard_returns],
-                                                    axis=1)
-        df_all_weighted_avg_standard_returns.rename(columns={0: sim_name}, inplace=True)
+            # Convert standard returns back to log returns
+            df_weighted_avg_log_returns = convert_returns(df_weighted_avg_standard_returns)
 
-    # calculate the default probability of the overall portfolio
-    df_weighted_avg_log_threshold_viol = pd.DataFrame()
-    df_weighted_avg_log_default_prob = pd.DataFrame(columns=["default_prob_portf"])
-    for threshold in THRESHOLDS_LOG:
-        df_defaults_portf = calc_threshold_violation(df_all_weighted_avg_log_returns, threshold)
-        df_weighted_avg_log_threshold_viol = pd.concat([df_weighted_avg_log_threshold_viol, df_defaults_portf])
+            # aggregate all simulated portfolio log returns into one df
+            sim_name = f"Simulation {i + 1}"
+            df_all_weighted_avg_log_returns = pd.concat([df_all_weighted_avg_log_returns, df_weighted_avg_log_returns],
+                                                        axis=1)
+            df_all_weighted_avg_log_returns.rename(columns={0: sim_name}, inplace=True)
 
-    df_weighted_avg_log_default_prob["default_prob_portf"] = df_weighted_avg_log_threshold_viol.sum(axis=1) / SIM_NUMBER
-    df_weighted_avg_log_default_prob.index = [f"Threshold_{threshold * 100}" for threshold in THRESHOLDS]
+            # aggregate all simulated portfolio standard returns into one df
+            df_all_weighted_avg_standard_returns = pd.concat([df_all_weighted_avg_standard_returns, df_weighted_avg_standard_returns],
+                                                        axis=1)
+            df_all_weighted_avg_standard_returns.rename(columns={0: sim_name}, inplace=True)
 
-    # calculate the CVaR
-    CVaR_estimations = []
-    for level in CVAR_LEVEL:
-        CVaR = get_CVaR(df_all_weighted_avg_standard_returns, level)
-        CVaR_estimations.append(CVaR)
+        # calculate the default probability of the overall portfolio
+        df_weighted_avg_log_threshold_viol = pd.DataFrame()
+        df_weighted_avg_log_default_prob = pd.DataFrame(columns=["default_prob_portf"])
+        for threshold in THRESHOLDS_LOG:
+            df_defaults_portf = calc_threshold_violation(df_all_weighted_avg_log_returns, threshold)
+            df_weighted_avg_log_threshold_viol = pd.concat([df_weighted_avg_log_threshold_viol, df_defaults_portf])
 
-    CVaR = pd.DataFrame({'CVaR_{}%'.format(100 - level): [cvar] for level, cvar in zip(CVAR_LEVEL, CVaR_estimations)})
-    print("End")
+        df_weighted_avg_log_default_prob["default_prob_portf"] = df_weighted_avg_log_threshold_viol.sum(axis=1) / SIM_NUMBER
+        df_weighted_avg_log_default_prob.index = [f"Threshold_{threshold * 100}" for threshold in THRESHOLDS]
+
+        # calculate the CVaR
+        CVaR_estimations = []
+        for level in CVAR_LEVEL:
+            CVaR = get_CVaR(df_all_weighted_avg_standard_returns, level)
+            CVaR_estimations.append(CVaR)
+
+        CVaR = pd.DataFrame({'CVaR_{}%'.format(100 - level): [cvar] for level, cvar in zip(CVAR_LEVEL, CVaR_estimations)})
+
+        df_all_CVaR = pd.concat([df_all_CVaR, CVaR])
+        df_all_default_prob_port = pd.concat([df_all_default_prob_port, df_weighted_avg_log_default_prob], axis=1)
+
+        df_all_CVaR.to_excel("df_all_CVaR.xlsx")
+        df_all_default_prob_port.to_excel("df_all_default_prob_port.xlsx")
