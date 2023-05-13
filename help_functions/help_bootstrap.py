0a7d08a91cc4fe99e8fa28f41490fce423ffa2f9
OptAlex <82665203+OptAlex@users.noreply.github.com>
2023-05-13 23:01:23 +0200

    adjust help_bootstrapping

--- help_functions/help_bootstrap.py
+++ help_functions/help_bootstrap.py
@@ -1,22 +1,18 @@
 import pandas as pd
 import math
-from recombinator.block_bootstrap import circular_block_bootstrap
+from recombinator.block_bootstrap import moving_block_bootstrap
 from recombinator.optimal_block_length import optimal_block_length
 from help_functions.help_functions_default_prob import get_fund_dict
 
 
-def bootstrap_returns(
-        df: pd.DataFrame,
-        bootstrap_samples: int,
-        bootstrap_days: int,
-) -> pd.DataFrame:
+def bootstrap_returns(df, bootstrap_samples=1):
     """
     This functions creates a bootstrap subsample for the log returns of each fund.
     :param df: df with historical log returns.
     :param bootstrap_samples: number of bootstrap samples the function creates.
-    :param bootstrap_days: number of days each bootstrap sample has.
     :return: df with bootstrapped returns.
     """
+
     # Create an empty DataFrame to store the bootstrapped returns
     bootstrap_df = pd.DataFrame()
 
@@ -29,21 +25,15 @@ def bootstrap_returns(
         b_star_cb = math.ceil(b_star[0].b_star_cb)
 
         # Perform bootstrapping of the data
-        bootstrap_returns = circular_block_bootstrap(
+        bootstrap_returns = moving_block_bootstrap(
             returns,
             block_length=b_star_cb,
             replications=bootstrap_samples,
-            sub_sample_length=bootstrap_days,
             replace=True,
         )
 
-        # Reshape the bootstrap returns from (1:252) to (252:1)
-        bootstrap_returns = bootstrap_returns.reshape(-1, 1)
-
-        # Convert the bootstrapped returns to a DataFrame
-        bootstrap_returns_df = pd.DataFrame(bootstrap_returns, columns=[f"{fund_name}"])
-
-        # Append the bootstrapped returns to the main DataFrame
+        bootstrap_returns_reshape = bootstrap_returns.reshape(-1, 1)
+        bootstrap_returns_df = pd.DataFrame(bootstrap_returns_reshape, columns=[f"{fund_name}"])
         bootstrap_df = pd.concat([bootstrap_df, bootstrap_returns_df], axis=1)
 
     return bootstrap_df
