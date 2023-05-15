342402b1d3d89334703e302f6311ce609e0abb9e
Tim Müller <timmuller@MacBook-Pro-von-Tim.local>
2023-05-15 11:21:27 +0200

    Added a function to calculate the confidence interval and implemented it to get_CVaR.

--- help_functions/help_CVaR.py
+++ help_functions/help_CVaR.py
@@ -1,15 +1,16 @@
-import pandas as pd
 import numpy as np
+from help_functions.help_confidence_interval import get_confidence_interval
 
 
-def get_CVaR(df, level, stop_loss, threshold):
+def get_CVaR(df, level, stop_loss, threshold, confidence_level):
     """
     Get the CVaR out of the pnl simulations.
     :param df: DataFrame with simulated pnl
     :param level: Level to estimate the CVaR
     :param stop_loss: Defines a stop loss for the portfolio
     :param threshold: Defines the threshold of the stop loss
-    :return: CVaR for the specified percentile of the pnl.
+    :param confidence_level: The confidence level in percentage (default to 95)
+    :return: A tuple (CVaR, (lower_ci, upper_ci)) representing the CVaR value and its confidence interval.
     """
     cumulative_returns = np.cumprod(df + 1, axis=0) - 1
     if stop_loss == True:
@@ -18,5 +19,7 @@ def get_CVaR(df, level, stop_loss, threshold):
     else:
         final_returns = cumulative_returns[-1, :]
     CVaR = np.percentile(a=final_returns, q=level)
-    return CVaR
+    lower_ci, upper_ci = get_confidence_interval(final_returns, confidence_level)
+
+    return CVaR, (lower_ci, upper_ci)
 
