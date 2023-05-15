3d5ae46509b229e5c6808f172ca2ebcd50fa6e3f
Tim Müller <timmuller@MacBook-Pro-von-Tim.local>
2023-05-15 10:58:24 +0200

    modified the CVaR function with stop loss feature.

--- help_functions/help_CVaR.py
+++ help_functions/help_CVaR.py
@@ -2,14 +2,19 @@ import pandas as pd
 import numpy as np
 
 
-def get_CVaR(df, level):
+def get_CVaR(df, level, stop_loss, threshold):
     """
     Get the CVaR out of the pnl simulations.
     :param df: DataFrame with simulated pnl
     :param level: level to estimate the CVaR
     :return: CVaR for the specified percentile of the pnl.
     """
-    # pnl = df.sum(axis=0)
-    cumulative_returns = np.cumprod(df + 1) - 1
-    CVaR = np.percentile(a=cumulative_returns, q=level)
+    cumulative_returns = np.cumprod(df + 1, axis=0) - 1
+    if stop_loss == True:
+        final_returns = np.where(cumulative_returns <= -threshold, -threshold, cumulative_returns)
+        final_returns = final_returns[-1, :]
+    else:
+        final_returns = cumulative_returns[-1, :]
+    CVaR = np.percentile(a=final_returns, q=level)
     return CVaR
+
